a0128a6506fd80fdd9790c264bd25c00ee87fdf6
Andrew Leonard <leonardishere@gmail.com>
2022-04-28 02:05:57 -0700

    set arguments

--- scripts/coverageDataProvider/polygon_arguments.js
+++ scripts/coverageDataProvider/polygon_arguments.js
@@ -1,5 +1,5 @@
 module.exports = [
-    "", // governor
+    "0x501AcE0e8D16B92236763E2dEd7aE3bc2DFfA276", // governor
     "0x7d43AABC515C356145049227CeE54B608342c0ad", // celer message bus
-    "", // coverage data provider
+    "0x501ACe6D80111c9B54FA36EEC5f1B213d7F24770", // coverage data provider
 ];
